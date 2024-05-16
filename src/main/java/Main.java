276ff432cdadcce65be9f396eb0bff7c23c6135b
Prem9910-p <prem60866@gmail.com>
2024-05-16 17:46:57 +0530

    solved

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Product p=new Product();
+        System.out.println(p.product(4,5));
+        System.out.println(p.product(4,5,6));
+        System.out.println(p.product(2.5,5.5));
+    }
+}
