7504e72875606a89d36c85e4cd87e4a471a56b75
Prem9910-p <prem60866@gmail.com>
2024-05-17 12:14:03 +0530

    ok done

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Product p=new Product();
-        System.out.println(p.product(4,5));
-        System.out.println(p.product(4,5,6));
-        System.out.println(p.product(2.5,5.5));
-    }
-}
